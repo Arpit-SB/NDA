398343934bb95331d6e0ad285a2f17797abf56dc
Arpit <prajapati7468@gmail.com>
2026-09-26 15:16:05 +0530

    Update Supabase URL to remove '/rest/v1/'

--- js/supabase.js
+++ js/supabase.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://wzifazimvyvicqtjybzs.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://wzifazimvyvicqtjybzs.supabase.co";
 const SUPABASE_KEY = "sb_publishable_5bUy-cmxbu5vR4JkTUSHdA_c1dFRLsH";
 
 const supabaseClient = supabase.createClient(
