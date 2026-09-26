4cab34c7528f0bc11c1cc581b35f301a202d0810
Arpit <prajapati7468@gmail.com>
2026-09-26 14:56:01 +0530

    Initialize Supabase client in supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://wzifazimvyvicqtjybzs.supabase.co/rest/v1/";
+const SUPABASE_KEY = "sb_publishable_5bUy-cmxbu5vR4JkTUSHdA_c1dFRLsH";
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
